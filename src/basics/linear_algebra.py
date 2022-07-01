8c72d9daf9d03436f99b70bea0c3b520b5a2caf0
Vaghinak <vaghinak.vardanyan@portmind.com>
2022-07-02 03:23:23 +0400

    linear algebra additions

--- src/basics/linear_algebra.py
+++ src/basics/linear_algebra.py
@@ -1,10 +1,11 @@
 import math
-from typing import List
-from typing import Tuple
-from typing import Callable
+import random
+from typing import List, Tuple, Callable
+
 
 Vector = List[float]
 Matrix = List[List[float]]
+
 
 def add(v: Vector, w: Vector) -> Vector:
     """Adds corresponding elements"""
@@ -106,3 +107,39 @@ def identity_matrix(n: int) -> Matrix:
     """Returns the n x n identity matrix"""
     return make_matrix(n, n, lambda i, j: 1 if i == j else 0)
 
+
+def matrix_times_matrix(m1: Matrix, m2: Matrix) -> Matrix:
+    nr1, nc1 = shape(m1)
+    nr2, nc2 = shape(m2)
+    assert nc1 == nr2, "must have (# of columns in m1) == (# of rows in m2)"
+
+    def entry_fn(i: int, j: int) -> float:
+        """dot product of i-th row of m1 with j-th column of m2"""
+        return sum(m1[i][k] * m2[k][j] for k in range(nc1))
+
+    return make_matrix(nr1, nc2, entry_fn)
+
+
+def matrix_times_vector(m: Matrix, v: Vector) -> Vector:
+    nr, nc = shape(m)
+    n = len(v)
+    assert nc == n, "must have (# of cols in m) == (# of elements in v)"
+
+    return [dot(row, v) for row in m]  # output has length nr
+
+
+# can run forever
+def find_eigenvector(m: Matrix,
+                     tolerance: float = 0.00001) -> Tuple[Vector, float]:
+    guess = [random.random() for _ in m]
+
+    while True:
+        result = matrix_times_vector(m, guess)    # transform guess
+        norm = magnitude(result)                  # compute norm
+        next_guess = [x / norm for x in result]   # rescale
+
+        if distance(guess, next_guess) < tolerance:
+            # convergence so return (eigenvector, eigenvalue)
+            return next_guess, norm
+
+        guess = next_guess
