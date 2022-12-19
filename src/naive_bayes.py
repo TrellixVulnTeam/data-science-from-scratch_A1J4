ce3c9ff8a9cc4b3ebbac6002a46c3a0092dda7e3
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-19 01:32:49 +0000

    Adding tarfile member sanitization to extractall()

--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -114,7 +114,26 @@ def download_data(output_dir: str):
         fin = BytesIO(content)
         # And extract all the files to the specified output dir.
         with tarfile.open(fileobj=fin, mode='r:bz2') as tf:
-           tf.extractall(output_dir)
+           def is_within_directory(directory, target):
+               
+               abs_directory = os.path.abspath(directory)
+               abs_target = os.path.abspath(target)
+           
+               prefix = os.path.commonprefix([abs_directory, abs_target])
+               
+               return prefix == abs_directory
+           
+           def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+           
+               for member in tar.getmembers():
+                   member_path = os.path.join(path, member.name)
+                   if not is_within_directory(path, member_path):
+                       raise Exception("Attempted Path Traversal in Tar File")
+           
+               tar.extractall(path, members, numeric_owner=numeric_owner) 
+               
+           
+           safe_extract(tf, output_dir)
 
 
 def preprocess_data(path: str):
